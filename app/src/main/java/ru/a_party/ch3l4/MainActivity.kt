e01fa90a8bb60fc01e297e11e30a5c939fa6fcb2
andreysc1976 <andreysc@yandex.ru>
2021-12-20 17:49:47 +0300

    Initial commit

---
+++ app/src/main/java/ru/a_party/ch3l4/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.a_party.ch3l4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
